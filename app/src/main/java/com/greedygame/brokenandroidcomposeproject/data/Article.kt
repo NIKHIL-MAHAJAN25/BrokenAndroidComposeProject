262a67501ec5380e983f6235de979676f615851a
Nikhil mahajan <nikhilmahajan8787@gmail.com>
2026-01-11 00:37:01 +0530

    updated data classes

--- app/src/main/java/com/greedygame/brokenandroidcomposeproject/data/Article.kt
+++ app/src/main/java/com/greedygame/brokenandroidcomposeproject/data/Article.kt
@@ -3,13 +3,22 @@ package com.greedygame.brokenandroidcomposeproject.data
 import com.google.gson.Gson
 
 data class Article(
-    val id: Int,
-    val title: String,
-    val author: String?,
-    val content: String?,
-    val imageUrl: String?
+   val source:Sources?,
+    val author:String?,
+    val title:String?,
+    val content:String?,
+    val urlToImage:String?
 )
-
+data class ArticleResponse(
+    val article:List<Article>,
+    val status:String,
+    val code:String?,
+    val message:String?
+)
+data class Sources(
+    val id:String?,
+    val name:String?
+)
 //object BrokenRepository {
 //    fun fetchArticlesBlocking(): List<Article> {
 //        Thread.sleep(2000)
